4aa06d1200a2ea16784f3a3ac5d6997df93643a0
Maria Dimon <momeria.dimon@gmail.com>
2022-05-03 08:13:50 +0200

    HTML til login side oprettet, mangler js, css og database

---
+++ healthtrackr/test/login.php
@@ -0,0 +1,27 @@
+<?php
+require("mysqlconnect.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <header>
+        <img src="#" alt="Logo, Health Trackr Logo">
+    </header>
+    <section>
+        <form action="">
+            <input type="text" placeholder="Navn">
+            <input type="text" placeholder="Password">
+            <button class="rund-button-1">Login</button>
+        </form>
+    </section>
+    <footer>
+        <p>Sundhedsfagling?</p>
+    </footer>
+</body>
+</html>
