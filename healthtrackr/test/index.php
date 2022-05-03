a2927717f8b59362c0f6a500d7893f935d43209f
Maria Dimon <momeria.dimon@gmail.com>
2022-05-03 08:10:25 +0200

    Forside html, i php fil lavet. mangler style

---
+++ healthtrackr/test/index.php
@@ -0,0 +1,24 @@
+<?php
+require("mysqlconnect.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forside</title>
+</head>
+<body>
+    <section>
+        <div class="background">
+            <button class="rund-button-1">Login</button>
+            <p>Ny bruger?</p>
+            <p><b>Opret bruger</b></p>
+        </div>
+    </section>
+    <footer>
+        <p>Sundhedsfagling?</p>
+    </footer>
+</body>
+</html>
