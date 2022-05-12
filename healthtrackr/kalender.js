46ff4c52a3923e8346c677480f85e636b5e4d2cc
Mohamed <83807211+MohamedAbdisamed@users.noreply.github.com>
2022-05-12 11:07:16 +0200

    rettet i både css og js filen for at fordele koderne til de rigtige file
    Jeg havde oprettet det hele i en fil.. nu har jeg fordelt koden og gjort den mere læsebar

--- healthtrackr/kalender.js
+++ healthtrackr/kalender.js
@@ -1,82 +1,44 @@
-const date = new Date();
-
-const renderCalendar = () => {
-  date.setDate(1);
-
-  const monthDays = document.querySelector(".days");
-
-  const lastDay = new Date(
-    date.getFullYear(),
-    date.getMonth() + 1,
-    0
-  ).getDate();
-
-  const prevLastDay = new Date(
-    date.getFullYear(),
-    date.getMonth(),
-    0
-  ).getDate();
-
-  const firstDayIndex = date.getDay();
-
-  const lastDayIndex = new Date(
-    date.getFullYear(),
-    date.getMonth() + 1,
-    0
-  ).getDay();
-
-  const nextDays = 7 - lastDayIndex - 1;
-
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
-  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
-
-  document.querySelector(".date p").innerHTML = new Date().toDateString();
-
-  let days = "";
-
-  for (let x = firstDayIndex; x > 0; x--) {
-    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
-  }
-
-  for (let i = 1; i <= lastDay; i++) {
-    if (
-      i === new Date().getDate() &&
-      date.getMonth() === new Date().getMonth()
-    ) {
-      days += `<div class="today">${i}</div>`;
-    } else {
-      days += `<div>${i}</div>`;
-    }
-  }
-
-  for (let j = 1; j <= nextDays; j++) {
-    days += `<div class="next-date">${j}</div>`;
-    monthDays.innerHTML = days;
-  }
-};
-
-document.querySelector(".prev").addEventListener("click", () => {
-  date.setMonth(date.getMonth() - 1);
-  renderCalendar();
+document.addEventListener('DOMContentLoaded', function () {
+  var calendarEl = document.getElementById('calendar');
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    timeZone: 'UTC',
+    themeSystem: 'bootstrap5',
+    headerToolbar: {
+      left: 'prev',
+      center: 'title',
+      right: 'next'
+    },
+    weekNumbers: true,
+    dayMaxEvents: true, // allow "more" link when too many events
+    events: 'https://fullcalendar.io/api/demo-feeds/events.json',
+    locale: 'da',
+    eventColor: '#378006',
+    events: [
+          {
+          title: 'TEST event',
+          start: '2022-05-10T10:30:00',
+          end: '2022-05-12T11:30:00',
+          description: 'Test1'
+          }, 
+          {
+          title: 'TEST event 2',
+          start: '2022-05-20T10:30:00',
+          end: '2022-05-21T11:30:00',
+          description: 'Test2'
+          }, 
+      ],
+      selectable:true,
+      select:function(start, end, allDays){
+         $('#exampleModal').modal('toggle');
+         $('#btnSave').click(function(){
+          var message = $('#message-text').val();
+          var start_date = moment(start).format('YYYY-MM-DD');
+          var end_date = moment(end).format('YYYY-MM-DD');
+          console.log(message)
+          console.log(start_date + ' - ' + end_date)
+         });
+      }
+    
+  });
+  calendar.render();
 });
-
-document.querySelector(".next").addEventListener("click", () => {
-  date.setMonth(date.getMonth() + 1);
-  renderCalendar();
-});
-
-renderCalendar();
