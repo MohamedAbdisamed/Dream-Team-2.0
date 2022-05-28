6e55427933d703b4078ddb070af7af4a21c11da9
Maria Nordahl Dimon <momeria.dimon@gmail.com>
2022-05-28 17:15:58 +0200

    Login fejl besked rettet til dansk

--- healthtrackr_produkt/src/index.php
+++ healthtrackr_produkt/src/index.php
@@ -24,7 +24,7 @@
                                     <br><br>
                                     <h2 class=" fw-bold mb-2 text-uppercase">Login</h2>
                                     <?php if (isset($_GET['err'])) { ?>
-                                        <div class="alert alert-danger text-center"><?php echo "Login failed! Invalid email-id or password!"; ?></div>
+                                        <div class="alert alert-danger text-center"><?php echo "Kunne ikke logge ind. Forkert email eller password."; ?></div>
                                     <?php } ?>
                                     <div class="form-outline form-white mb-4">
                                         <input type="email" id="typeEmailX" required class="form-control form-control-lg" placeholder="Email" name="email" />
