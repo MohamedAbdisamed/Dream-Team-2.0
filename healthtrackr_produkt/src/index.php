569ffec7064056403705ad9b305e6e4240b644ec
Maria Dimon <momeria.dimon@gmail.com>
2022-05-30 13:45:49 +0200

    Video fjernet

--- healthtrackr_produkt/src/index.php
+++ healthtrackr_produkt/src/index.php
@@ -17,7 +17,6 @@
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-12 col-md-8 col-lg-6 col-xl-5">
                     <div id="login" class="card text-white" style="border-radius: 1rem;">
-                    <video class="position-fixed w-100"autoplay loop src="../scss/video/forside.mp4" style="border-radius: 1rem;"></video>
                         <form id="login-form" method="post" action="login.php" role="form">
                             <div class="card-body p-5 text-center">
                                 <div class="mb-md-5 mt-md-4 pb-5">
