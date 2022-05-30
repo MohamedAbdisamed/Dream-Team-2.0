02ac8b5a8623b43f1d007df10b43a84047a9b9c7
Maria Dimon <momeria.dimon@gmail.com>
2022-05-30 13:23:23 +0200

    Top og bot nav redigeret

--- healthtrackr_produkt/src/botnav.php
+++ healthtrackr_produkt/src/botnav.php
@@ -1,46 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../scss/custom.scss">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
+
 <body>
     <!-- Bot Nav -->
-    <nav class="navbar navbar-expand-lg sticky-bottom" style="margin-top: 10px; background-color:#F2F1ED">
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-bottom">
         <div class="container-fluid">
             <a class="navbar-brand" href="home.php">
-                <span class="material-symbols-rounded" style="color: white; padding: 5px;">
-                    <img src="../scss/img/nav-icons/home-icon.png" />
+                <span class="material-symbols-rounded">
+                    home
                 </span>
             </a>
-            <a class="navbar-brand" href="#">
-                <span class="material-symbols-rounded" style="color: white; padding: 5px;">
-                    <img src="../scss/img/nav-icons/chat-icon.png" />
+            <a class="navbar-brand" href="chat-overview.php">
+                <span class="material-symbols-rounded">
+                    forum
                 </span>
             </a>
             <a class="navbar-brand" href="programcat.php">
-                <span class="material-symbols-rounded" style="color: white; padding: 5px;">
-                    <img src="../scss/img/nav-icons/workout-icon.png" />
+                <span class="material-symbols-rounded">
+                    fitness_center
                 </span>
             </a>
             <a class="navbar-brand" href="kalender.php">
-                <span class="material-symbols-rounded" style="color: white;padding: 5px;">
-                    <img src="../scss/img/nav-icons/calender-icon.png" />
+                <span class="material-symbols-rounded">
+                    calendar_month
                 </span>
             </a>
             <a class="navbar-brand" href="user.php">
-                <span class="material-symbols-rounded" style="color: white; padding: 5px;">
-                    <img src="../scss/img/nav-icons/user-icon.png" />
+                <span class="material-symbols-rounded">
+                    person
                 </span>
             </a>
         </div>
     </nav>
-  <!--  <script src="footer.php"></script> -->
+    <!--  <script src="footer.php"></script> -->
     <script type="module" src="./index.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
+
 </html>
--- healthtrackr_produkt/src/topnav.php
+++ healthtrackr_produkt/src/topnav.php
@@ -1,8 +1,9 @@
 <?php
 include('session.php');
- ?>
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,48 +11,52 @@ include('session.php');
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
+
 <body>
     <!-- Top Nav -->
-    <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
         <div class="container-fluid">
-            <a href="home.php"><img src="../scss/img/nav-icons/health_trackr_logo.png" width="90"/> </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="material-symbols-rounded" style="color: white; padding: 5px;">
-                <span class="navbar-toggler-icon"></span>
+            <a href="home.php"><img src="../scss/img/nav-icons/health_trackr_logo.png" width="90" /> </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="material-symbols-rounded">
+                    menu
                 </span>
             </button>
             <div class="collapse navbar-collapse " id="navbarNav">
                 <ul class="nav navbar-nav ml-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="user.php">
-                            <span class="material-symbols-rounded" style="color: white;padding: 5px;">
-                                Profil
+                            <span class="material-symbols-rounded">
+                                person
                             </span>
                         </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="kalender.php">
-                            <span class="material-symbols-rounded" style="color: white;padding: 5px;">
-                                Kalender
+                            <span class="material-symbols-rounded">
+                                calendar_month
                             </span>
                         </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="programcat.php">
-                            <span class="material-symbols-rounded" style="color: white;padding: 5px;">
-                                Trænings programmer
+                            <span class="material-symbols-rounded">
+                                fitness_center
                             </span>
                         </a>
-                    </li> 
+                    </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="logout.php">Sign Out</a>
-                    </li> 
+                        <a class="nav-link" href="logout.php"><span class="material-symbols-rounded">
+                                logout
+                            </span></a>
+                    </li>
                 </ul>
             </div>
         </div>
-    </nav> 
+    </nav>
     <script type="module" src="index.js"></script>
-     
+
 </body>
 
 </html>
