0c8f1521be0a1f6bc6da6d5f4c1330a6135ca0f8
Maria Dimon <momeria.dimon@gmail.com>
2022-05-30 08:23:15 +0200

    Chat overview opsat

--- healthtrackr_produkt/src/chat-overview.php
+++ healthtrackr_produkt/src/chat-overview.php
@@ -12,6 +12,44 @@
     <!-- Top Nav -->
     <?php include 'topnav.php'; ?>
 
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.6.8-fix/jquery.nicescroll.min.js"></script>
+    <div class="container">
+        <div class="content container-fluid bootstrap snippets bootdey">
+            <div class="col-sm-3 col-xs-12">
+                <div class="col-inside-lg decor-default chat" style="overflow: hidden; outline: none;" tabindex="5000">
+                    <div class="chat-users">
+                        <h6>Online</h6>
+                        <div class="user"><a href="chat.php">
+                            <div class="avatar">
+                                <img src="../scss/img/fys.JPG" alt="User name">
+                                <div class="status off"></div>
+                            </div>
+                            <div class="name">User name</div>
+                            <div class="mood">User mood</div>
+                        </a></div>
+                        <div class="user">
+                            <div class="avatar">
+                                <img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="User name">
+                                <div class="status online"></div>
+                            </div>
+                            <div class="name">User name</div>
+                            <div class="mood">User mood</div>
+                        </div>
+                        <div class="user">
+                            <div class="avatar">
+                                <img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="User name">
+                                <div class="status busy"></div>
+                            </div>
+                            <div class="name">User name</div>
+                            <div class="mood">User mood</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
     <!-- Bot Nav -->
     <?php include 'botnav.php'; ?>
