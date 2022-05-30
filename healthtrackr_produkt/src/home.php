d5a16693c64bc32d6d67f4c202ab9bb02d2de64e
Mohamed <83807211+MohamedAbdisamed@users.noreply.github.com>
2022-05-30 13:54:28 +0200

    opdateret billeder

--- healthtrackr_produkt/src/home.php
+++ healthtrackr_produkt/src/home.php
@@ -18,7 +18,7 @@
     -->
     <header>
         <div class="overlay"></div>
-        <img src="../scss/img/profil.jpg" alt="Bruger profil billed">
+        <img src="../scss/img/profil.jpg" style="object-position:80% 100%;" alt="Bruger profil billed">
         <div class="container h-100">
             <div class="d-flex h-100 text-center align-items-center">
                 <div class="w-100 text-white">
