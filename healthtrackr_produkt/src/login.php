6159edfed39178dcecbccaf1dd0a3094a7085600
Mohamed <83807211+MohamedAbdisamed@users.noreply.github.com>
2022-05-30 20:07:13 +0200

    rettet login fejl

--- healthtrackr_produkt/src/login.php
+++ healthtrackr_produkt/src/login.php
@@ -6,7 +6,7 @@ if (isset($_POST['submit'])) {
     // username and password sent from form 
    
    $email = mysqli_real_escape_string($conn,$_POST['email']);
-   $password = mysqli_real_escape_string($conn,$_POST['pass']); 
+   $password = mysqli_real_escape_string($conn,$_POST['password']); 
    
    $sql = "SELECT email, pass FROM Users WHERE email = '$email' and pass = '$password'";
    $result = mysqli_query($conn,$sql);
